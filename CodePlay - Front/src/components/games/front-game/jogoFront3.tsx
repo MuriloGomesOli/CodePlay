fc87d9730d2601e8fd4bf22b0b54a99f468aa59b
Murilo Gomes de Oliveira <159261576+MuriloGomesOli@users.noreply.github.com>
2025-10-21 16:48:05 -0300

    front2

--- CodePlay - Front/src/components/games/front-game/jogoFront3.tsx
+++ CodePlay - Front/src/components/games/front-game/jogoFront3.tsx
@@ -10,10 +10,31 @@ import styles from '../../../styles/jogo.module.css';
 const App: React.FC = () => {
   return (
     <div className={styles.appContainer}>
-      <UserProfile />
-      <CodeEditor />
-      <GameView />
-
+      <UserProfile
+        userName="Lola"
+        userTitle="Programadora Iniciante"
+        avatarSrc="/assets/Lola.png"
+        skills={['CSS', 'Flexbox']}
+        module={1}
+        level={1}
+        onProfileClick={() => console.log('Abrir perfil')}
+        onMenuClick={() => console.log('Abrir menu')}
+        onLogoutClick={() => console.log('Sair do jogo')}
+/>
+      <CodeEditor
+        welcomeText="🐮 Bem-vindo ao Nível 2 do Code Play! Hora de dar vida à fazenda com movimento e estilo."
+        instructionText="Escreva um comando para aprender sobre movimentos e montar a fazenda. Use <code>position</code> diferente de <code>static</code>."
+        codeExample="/* Exemplo de comando CSS */"
+        hintText="top: 10px;<br/>left: 5px;<br/>bottom: 20px;<br/>right: 5px;"
+        mainButtonText="CONFRIMAR"
+        onNext={() => console.log('Próximo passo!')}
+/>
+      <GameView 
+        falaPersonagem="Olá! Vamos montar minha fazenda juntos?"
+        fundo= {Fazenda}
+        personagem={Fazendeiro}
+        extra={Trator}
+      />
     </div>
   );
 };
